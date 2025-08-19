3412841deabc82015bfa040a05c6fc0b0f3abf64
ZakharDay <zakhar.day@gmail.com>
2025-08-19 22:49:57 +0300

    Module 2 Lesson 6 P5

--- module_2_lesson_6_2/script.js
+++ module_2_lesson_6_2/script.js
@@ -1,3 +1,7 @@
+//
+// UTILITIES
+//
+
 function getRandomInt(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
@@ -5,19 +9,20 @@ function getRandomInt(min, max) {
 }
 
 //
+// UTILITIES END
 //
+
+//
+// VARS
 //
 
 const minSize = 20;
 const maxSize = 60;
 
-let currentFrame = 0;
-let framesTillNextCollision = 0;
 let elementVerticalDirection = 'down';
 let elementHorizontalDirection = 'right';
 let elementSizeDirection = 'up';
-// let size = 20;
-let speed, frequency;
+let framesTillNextCollision, speed, frequency;
 
 const canvas = {
   w: 600,
@@ -30,15 +35,35 @@ const figure = {
   r: 20,
 };
 
+//
+// VARS END
+//
+
+//
+// P5
+//
+
 function setup() {
   createCanvas(canvas.w, canvas.h);
   speed = 1;
+  frequency = 3;
 }
 
 function draw() {
-  background(135, 206, 235);
   changeElementPosition();
+  changeElementSize();
+
+  background(135, 206, 235);
+  circle(figure.x, figure.y, figure.r);
 }
+
+//
+// P5 END
+//
+
+//
+// JS
+//
 
 function changeElementPosition() {
   const maxLeft = canvas.w - figure.r / 2;
@@ -61,197 +86,136 @@ function changeElementPosition() {
     }
   }
 
-  circle(figure.x, figure.y, figure.r);
-
   if (
     elementVerticalDirection == 'down' &&
-    figure.y + figure.r / 2 == canvas.h
+    figure.y + figure.r / 2 >= canvas.h
   ) {
     elementVerticalDirection = 'up';
 
     console.log('up');
 
-    // changeSpeed();
-    // changeFrequency();
-
-    // calcNextCollision(
-    //   containerWidth,
-    //   containerHeight,
-    //   containerLeft,
-    //   containerTop,
-    //   left,
-    //   top
-    // );
+    changeSpeed();
+    changeFrequency();
+    calcNextCollision();
   }
 
   if (
     elementHorizontalDirection == 'right' &&
-    figure.x + figure.r / 2 == canvas.w
+    figure.x + figure.r / 2 >= canvas.w
   ) {
     elementHorizontalDirection = 'left';
 
     console.log('left');
 
-    // changeSpeed();
-    // changeFrequency();
-
-    // calcNextCollision(
-    //   containerWidth,
-    //   containerHeight,
-    //   containerLeft,
-    //   containerTop,
-    //   left,
-    //   top
-    // );
+    changeSpeed();
+    changeFrequency();
+    calcNextCollision();
   }
 
-  if (elementVerticalDirection == 'up' && figure.y - figure.r / 2 == 0) {
+  if (elementVerticalDirection == 'up' && figure.y - figure.r / 2 <= 0) {
     elementVerticalDirection = 'down';
 
     console.log('down');
 
-    // changeSpeed();
-    // changeFrequency();
-
-    // calcNextCollision(
-    //   containerWidth,
-    //   containerHeight,
-    //   containerLeft,
-    //   containerTop,
-    //   left,
-    //   top
-    // );
+    changeSpeed();
+    changeFrequency();
+    calcNextCollision();
   }
 
-  if (elementHorizontalDirection == 'left' && figure.x - figure.r / 2 == 0) {
+  if (elementHorizontalDirection == 'left' && figure.x - figure.r / 2 <= 0) {
     elementHorizontalDirection = 'right';
 
     console.log('right');
 
-    // changeSpeed();
-    // changeFrequency();
-
-    // calcNextCollision(
-    //   containerWidth,
-    //   containerHeight,
-    //   containerLeft,
-    //   containerTop,
-    //   left,
-    //   top
-    // );
+    changeSpeed();
+    changeFrequency();
+    calcNextCollision();
   }
 }
 
+function changeElementSize() {
+  if (frequency && framesTillNextCollision) {
+    const framesInCycle = framesTillNextCollision / frequency / 2;
+    const pixelsInFrame = (maxSize - minSize) / framesInCycle;
+
+    if (elementSizeDirection == 'up') {
+      figure.r += parseInt(pixelsInFrame);
+    } else {
+      figure.r -= parseInt(pixelsInFrame);
+    }
+
+    if (figure.r >= maxSize) {
+      elementSizeDirection = 'down';
+    }
+
+    if (figure.r <= minSize) {
+      elementSizeDirection = 'up';
+    }
+  }
+}
+
+function calcNextCollision() {
+  const s = speed;
+  const currentFrame = frameCount;
+  let frame = frameCount;
+  let x = figure.x;
+  let y = figure.y;
+
+  let nextCollisionFrame;
+
+  while (nextCollisionFrame == undefined) {
+    if (elementVerticalDirection == 'up') {
+      y -= s;
+    } else {
+      y += s;
+    }
+
+    if (elementHorizontalDirection == 'left') {
+      x -= s;
+    } else {
+      x += s;
+    }
+
+    const collision = detectCollision(x, y);
+
+    if (collision == 1) {
+      frame++;
+      nextCollisionFrame = frame;
+      framesTillNextCollision = nextCollisionFrame - currentFrame;
+    } else {
+      frame++;
+    }
+  }
+}
+
+function detectCollision(x, y) {
+  if (elementVerticalDirection == 'down' && y + minSize / 2 >= canvas.h) {
+    return true;
+  }
+
+  if (elementHorizontalDirection == 'right' && x + minSize / 2 >= canvas.w) {
+    return true;
+  }
+
+  if (elementVerticalDirection == 'up' && y - minSize / 2 <= 0) {
+    return true;
+  }
+
+  if (elementHorizontalDirection == 'left' && x - minSize / 2 <= 0) {
+    return true;
+  }
+
+  return false;
+}
+
+function changeSpeed() {
+  speed = getRandomInt(1, 5);
+}
+
+function changeFrequency() {
+  frequency = getRandomInt(1, 7);
+}
+
 //
+// JS END
 //
-//
-//
-
-// function changeElementSize() {
-//   if (frequency) {
-//     const framesInCycle = framesTillNextCollision / frequency / 2;
-//     const pixelsInFrame = (maxSize - minSize) / framesInCycle;
-
-//     if (elementSizeDirection == 'up') {
-//       size += pixelsInFrame;
-//     } else {
-//       size -= pixelsInFrame;
-//     }
-
-//     if (size >= maxSize) {
-//       elementSizeDirection = 'down';
-//     }
-
-//     if (size <= minSize) {
-//       elementSizeDirection = 'up';
-//     }
-//   }
-// }
-
-// function changeSpeed() {
-//   speed = getRandomInt(1, 5);
-// }
-
-// function changeFrequency() {
-//   frequency = getRandomInt(1, 7);
-// }
-
-// function calcNextCollision(
-//   containerWidth,
-//   containerHeight,
-//   containerLeft,
-//   containerTop,
-//   left,
-//   top
-// ) {
-//   const s = speed;
-//   let frame = currentFrame;
-//   let x = left;
-//   let y = top;
-
-//   let nextCollisionFrame;
-
-//   while (nextCollisionFrame == undefined) {
-//     if (elementVerticalDirection == 'up') {
-//       y -= s;
-//     } else {
-//       y += s;
-//     }
-
-//     if (elementHorizontalDirection == 'left') {
-//       x -= s;
-//     } else {
-//       x += s;
-//     }
-
-//     const collision = detectCollision(
-//       containerWidth,
-//       containerHeight,
-//       containerLeft,
-//       containerTop,
-//       x,
-//       y
-//     );
-
-//     if (collision == 1) {
-//       frame++;
-//       nextCollisionFrame = frame;
-//       framesTillNextCollision = nextCollisionFrame - currentFrame;
-//     } else {
-//       frame++;
-//     }
-//   }
-// }
-
-// function detectCollision(
-//   containerWidth,
-//   containerHeight,
-//   containerLeft,
-//   containerTop,
-//   left,
-//   top
-// ) {
-//   if (
-//     elementVerticalDirection == 'down' &&
-//     top >= containerTop + containerHeight - size
-//   ) {
-//     return true;
-//   }
-
-//   if (
-//     elementHorizontalDirection == 'right' &&
-//     left >= containerLeft + containerWidth - size
-//   ) {
-//     return true;
-//   }
-
-//   if (elementVerticalDirection == 'up' && top <= containerTop) {
-//     return true;
-//   }
-
-//   if (elementHorizontalDirection == 'left' && left <= containerLeft) {
-//     return true;
-//   }
-
-//   return false;
-// }
